964becd8109bf4a8ed4523aff13c0dbd9717057e
Francisco de la Mata <francismocil@gmail.com>
2019-04-05 13:16:55 +0200

    Funcional con mongo

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var bodyParser  = require("body-parser");
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -14,13 +18,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());    //No es necesario
+//app.use(express.urlencoded({ extended: false }));  //No es necesario
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +44,13 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({message:err.message,error:err}); 
 });
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Xylons:Francisco1.@cursoweb-xpbvj.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
+mongoose.set('useCreateIndex', true);
 module.exports = app;
